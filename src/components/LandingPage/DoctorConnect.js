7ed6054075066d8379262deb06798a23a64d5307
FrontendEtherium <developeretherium@gmail.com>
2026-02-02 11:41:10 +0530

    Update DoctorConnect.js

--- src/components/LandingPage/DoctorConnect.js
+++ src/components/LandingPage/DoctorConnect.js
@@ -203,7 +203,8 @@ function DoctorConnect() {
           </div>
         </div>
 
-        <Footer />
+{/*   <Footer /> */}
+    <Footer hideCuresLinks={true} />
       </div>
 
       {/* Modal rendered using portal to document body */}
