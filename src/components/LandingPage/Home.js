f1780494432734d2b7a9538c2fee4b4a902870f3
FrontendEtherium <developeretherium@gmail.com>
2026-04-06 13:41:35 +0530

    Update Home.js

--- src/components/LandingPage/Home.js
+++ src/components/LandingPage/Home.js
@@ -81,28 +81,13 @@ function Home() {
         <TrendingSearches isMobile={isMobile} />
         <FeaturedBlogs isMobile={isMobile} />
 
-  {/*  <Link to="/doctor">
+    <Link to="/doctor">
           <img
             src={`${imgKitImagePath}/assets/img/bannersdestop-mobiles-06.jpg`}
             alt="Promo Banner"
             className="promo-banner"
           />
-        </Link> */}
-    <div className="promo-banner-wrapper">
-  <img
-    src={`${imgKitImagePath}/assets/img/bannersdestop-mobiles-06.jpg`}
-    alt="Promo Banner"
-    className="promo-banner"
-  />
-
-  {/* SAME BUTTON AS CAROUSEL */}
-  <button
-    className="doctor-patient-banner-btn-bannersdestop-mobiles-06"
-    onClick={() => (window.location.href = "/doctor-connect")}
-  >
-    Consult Now
-  </button>
-</div>
+        </Link> 
 
         <TrendingCures isMobile={isMobile} />
         <DoctorCures isMobile={isMobile} />
@@ -129,13 +114,6 @@ function Home() {
         <SubscriberComponent />
         <Footer />
          </div>
-      {/* ✅ YAHAN */}
-    <div
-      className="consultsticky-btn"
-      onClick={() => (window.location.href = "/doctor-connect")}
-    >
-      Consult Now
-    </div>
     </div>
   );
 }
